f3d6106450ff6f6f959461bd0814df22f218ae32
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-09-06 19:45:06 +0200

    Add randomColor functionality

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var randomName = 4;
+import { argv } from 'node:process';
+
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const rdnColor = randomColor();
+
+
+
+const layout = `#############################,#############################,#############################,#####                   #####,#####      ${rdnColor}      #####,#####                   #####,#############################,#############################,#############################`;
+console.log(chalk.hex(randomColor())(layout.split(',').join('\r\n')));
+
+
+if(argv[1]){
+      console.log(rdnColor);
+  }

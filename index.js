7499ca155e21cdaa92cc43f2cde6139f411f031e
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-09-11 21:18:11 +0200

    Clean dependencies and preflight test

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import { argv } from 'node:process';
-
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const rdnColor = randomColor({luminosity: argv[3], hue: argv[2]});
+const rdnColor = randomColor({ luminosity: argv[3], hue: argv[2] });
 
 const layout = `#############################,#############################,#############################,#####                   #####,#####      ${rdnColor}      #####,#####                   #####,#############################,#############################,#############################`;
 
 const printRandomColor = chalk.hex(rdnColor)(layout.split(',').join('\r\n'));
 
 console.log(printRandomColor);
- 

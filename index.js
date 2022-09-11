a7d4b8b140847fa930568f914d59d1c83514bede
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-09-11 20:55:36 +0200

    Add colorinput + clean up code

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ import { argv } from 'node:process';
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const rdnColor = randomColor();
+const rdnColor = randomColor({luminosity: argv[3], hue: argv[2]});
 
 const layout = `#############################,#############################,#############################,#####                   #####,#####      ${rdnColor}      #####,#####                   #####,#############################,#############################,#############################`;
 
 const printRandomColor = chalk.hex(rdnColor)(layout.split(',').join('\r\n'));
 
-
-if(argv[1]){
-     console.log(printRandomColor);
-  } else if (argv[2]) {
-    
-    const initialColor = {luminosity: 'light', hue: 'blue'};
-    initialColor.hue = argv[2];
-
-    console.log(randomColor(initialColor));
-  };
+console.log(printRandomColor);
+ 

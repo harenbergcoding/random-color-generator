51c91228a5e074090376bbbf2e1385e4df8ce075
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-10-31 12:27:47 +0100

    Refactored code, add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Node.js Random Color Generator
+
+CLI Application. Returns random colors based on user input. Possible inputs are: node index.js for random color node index.js color to specify a color and node index.js color luminosity to specify a color and luminosity.
+
+![](nodejs-random-color-generator.png)
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import { argv } from 'node:process';
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const rdnColor = randomColor({ luminosity: argv[3], hue: argv[2] });
+const randomColorInput = randomColor({ luminosity: argv[3], hue: argv[2] });
 
-const layout = `#############################,#############################,#############################,#####                   #####,#####      ${rdnColor}      #####,#####                   #####,#############################,#############################,#############################`;
+const layout = `#############################,#############################,#############################,#####                   #####,#####      ${randomColorInput}      #####,#####                   #####,#############################,#############################,#############################`;
 
-const printRandomColor = chalk.hex(rdnColor)(layout.split(',').join('\r\n'));
+const printRandomColor = chalk.hex(randomColorInput)(
+  layout.split(',').join('\r\n'),
+);
 
 console.log(printRandomColor);

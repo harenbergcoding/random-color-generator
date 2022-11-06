b1865b4ca3367cd6a2e01e06e82844839b32f749
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-11-06 13:40:53 +0100

    Add drone requirement

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
 const randomColorInput = randomColor({ luminosity: argv[3], hue: argv[2] });
-
-const layout = `#############################,#############################,#############################,#####                   #####,#####      ${randomColorInput}      #####,#####                   #####,#############################,#############################,#############################`;
+const layout = ` ,#############################,#############################,#############################,#####                   #####,#####      ${randomColorInput}      #####,#####                   #####,#############################,#############################,#############################`;
 
 const printRandomColor = chalk.hex(randomColorInput)(
   layout.split(',').join('\r\n'),
